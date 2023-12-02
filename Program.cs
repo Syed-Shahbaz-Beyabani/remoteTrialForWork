7fa42ae7b6527d3bbfaa6baee0cb33bf1e957632
shahbaz <shazawez76@gmail.com>
2023-12-02 07:22:32 +0530

    firstCommit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myGitApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("my git practice work");
+            count();
+            Console.WriteLine("ready to commit");
+        }
+        static void count()
+        {
+            foreach(int i in Enumerable.Range(0, 10)){
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
